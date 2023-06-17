4b37c5fe2f5f468b53f8ca3b282ba4f5ee23ea58
susu xiang <susuxiang@susudeMac-mini.local>
2023-06-17 15:29:45 -0400

    Config Apollo client

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
-import Header from "./components/Header";
+import Header from './components/Header';
+import Clients from './components/Clients';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5050/graphql',
+  cache: new InMemoryCache()
+});
 
 function App() {
   return (
     <>
-      <Header />
-      <div className="container">
-        <h1>Hello</h1>
-      </div>
+      <ApolloProvider client={client}>
+        <Header />
+        <div className="container">
+          <Clients />
+        </div>
+      </ApolloProvider>
     </>
   );
 }
--- server/index.js
+++ server/index.js
@@ -3,11 +3,14 @@ require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const connectDB = require('./config/db');
+const cors = require('cors');
 const port = process.env.PORT || 5050;
 
 const app = express();
 
 connectDB();
+
+app.use(cors());
 
 app.use(
     '/graphql', 
