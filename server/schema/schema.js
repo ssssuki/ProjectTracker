373dee7ba673ac5808ab0e5b43b6e935439fac07
susu xiang <susuxiang@susudeMac-mini.local>
2023-06-17 01:35:09 -0400

    Query MongoDB for project and client data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,3 @@
-const { projects, clients } = require('../sample.js');
-
 const Project = require('../models/Project');
 const Client = require('../models/Client');
 
@@ -25,7 +23,7 @@ const ProjectType = new GraphQLObjectType({
         client: {
             type: ClientType,
             resolve(parent, args) {
-                return clients.find(client => client.id === parent.clientId);
+                return Client.findById(parent.clientId);
             }
         }
     })
@@ -37,27 +35,27 @@ const RootQuery = new GraphQLObjectType({
         projects: {
             type: new GraphQLList(ProjectType),
             resolve(parent, args) {
-                return projects;
+                return Project.find();
             }
         },
         project: {
             type: ProjectType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-                return projects.find(project => project.id === args.id);
+                return Project.findById(args.id);
             }
         },
         clients: {
             type: new GraphQLList(ClientType),
             resolve(parent, args) {
-                return clients;
+                return Client.find();
             }
         },
         client: {
             type: ClientType,
             args: { id: { type: GraphQLID } },
             resolve(parent, args) {
-                return clients.find(client => client.id === args.id);
+                return Client.findById(args.id);
             }
         }
     }
