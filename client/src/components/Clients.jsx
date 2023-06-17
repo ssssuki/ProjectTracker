00709a09d41ec4c8059aa51c194dd60c369a41be
susu xiang <susuxiang@susudeMac-mini.local>
2023-06-17 15:36:31 -0400

    Display clients

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,4 +1,5 @@
 import { gql, useQuery } from '@apollo/client';
+import ClientRow from './ClientRow';
 
 const GET_CLIENTS = gql`
   query getClients {
@@ -24,7 +25,25 @@ export default function Clients() {
 
   return (
     <>
-
+      { !loading && !error && (
+        <table className='table table-hover mt-3'>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              data.clients.map(client => (
+                <ClientRow key={client.id} client={client} />
+              ))
+            }
+          </tbody>
+        </table>
+      ) }
     </>
   )
 }
