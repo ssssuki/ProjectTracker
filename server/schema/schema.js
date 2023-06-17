e778e0f4722136100dc5a7e16450bdcec3d92ddf
susu xiang <susuxiang@susudeMac-mini.local>
2023-06-17 01:30:28 -0400

    Add mongoose models

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,8 @@
 const { projects, clients } = require('../sample.js');
 
+const Project = require('../models/Project');
+const Client = require('../models/Client');
+
 const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList } = require('graphql')
 
 const ClientType = new GraphQLObjectType({
