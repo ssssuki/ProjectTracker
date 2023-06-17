122cc65b1cef379d06394a9e4efd689fd2868530
susu xiang <susuxiang@susudeMac-mini.local>
2023-06-17 01:21:37 -0400

    Add MongoDB connection config

--- server/index.js
+++ server/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5050;
 
 const app = express();
+
+connectDB();
 
 app.use(
     '/graphql', 
