ad28fb77b3755064c94cecf8e7b622a8583bcff0
susu xiang <susuxiang@susudeMac-mini.local>
2023-06-17 00:59:50 -0400

    Allow query for all clients

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,6 @@
 const { projects, clients } = require('../sample.js');
 
-const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema } = require('graphql')
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList } = require('graphql')
 
 const ClientType = new GraphQLObjectType({
     name: 'Client',
@@ -15,6 +15,12 @@ const ClientType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
+        clients: {
+            type: new GraphQLList(ClientType),
+            resolve(parent, args) {
+                return clients;
+            }
+        },
         client: {
             type: ClientType,
             args: { id: { type: GraphQLID } },
