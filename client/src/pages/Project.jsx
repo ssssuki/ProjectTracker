90aac2b8ecd10d6af67619a19fa6e03644037bd6
susu xiang <susuxiang@susudeMac-mini.local>
2023-06-18 01:33:09 -0400

    Small UI fix

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -27,7 +27,7 @@ export default function Project() {
           <Link to='/' className='btn btn-light btn-sm w-25 d-inline ms-auto'>
             Back
           </Link>
-          <h1>{ data.project.name }</h1>
+          <h1 className='mt-3'>{ data.project.name }</h1>
           <p>{ data.project.description }</p>
           <h5 className='mt-3'>Project Status</h5>
           <p>{ data.project.status }</p>
