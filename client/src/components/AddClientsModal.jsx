193b389affc1e9d8bdec4c566d1483a757bf33e5
susu xiang <susuxiang@susudeMac-mini.local>
2023-06-17 22:34:28 -0400

    Add project modal

--- client/src/components/AddClientsModal.jsx
+++ client/src/components/AddClientsModal.jsx
@@ -51,15 +51,15 @@ export default function AddClientsModal() {
               <form onSubmit={onSubmit}>
                 <div className='mb-3'>
                   <label className='form-label'>Name</label>
-                  <input type='text' className='form-control' id='name' value={name} onChange={(e) => setName(e.target.value)} />
+                  <input type='text' className='form-control' id='name' value={name} onChange={ e => setName(e.target.value) } />
                 </div>
                 <div className='mb-3'>
                   <label className='form-label'>Email</label>
-                  <input type='text' className='form-control' id='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+                  <input type='text' className='form-control' id='email' value={email} onChange={ e => setEmail(e.target.value) } />
                 </div>
                 <div className='mb-3'>
                   <label className='form-label'>Phone</label>
-                  <input type='text' className='form-control' id='phone' value={phone} onChange={(e) => setPhone(e.target.value)} />
+                  <input type='text' className='form-control' id='phone' value={phone} onChange={ e => setPhone(e.target.value) } />
                 </div>
                 <button type='submit' data-bs-dismiss='modal' className='btn btn-secondary'>
                   Submit
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Clients from '../components/Clients';
 import Projects from '../components/Projects';
 import AddClientsModal from '../components/AddClientsModal';
+import AddProjectsModal from '../components/AddProjectsModal';
 
 export default function Home() {
   return (
     <>
       <div className='d-flex gap-3 mb-4'>
         <AddClientsModal />
+        <AddProjectsModal />
       </div>
       <Projects />
       <hr />
